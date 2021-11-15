f3fb5dabb4ec42442e0fb2fa7df88154ec0123ed
OldGroof <39658757+OldGroof@users.noreply.github.com>
2021-11-15 18:57:32 +0000

    hmmm

--- Resources/da40.js
+++ Resources/da40.js
@@ -501,9 +501,9 @@ function perfTO() {
 
   // Calculate wind variation(windVar) based on headwind or tailwind
   if (wind >= 0) {
-    var windVar = (-1 * wind) / 12
+    var windVar = -1 * (wind / 12)
   } else {
-    var windVar = (-1 * wind) / 2
+    var windVar = -1 * (wind / 2)
   }
 
   var todr = (Math.floor((tod + ((0.1 * tod) * windVar)) + 0.5)) + 30 // +30m for no wheel fairings and wind correction (+-10% for each windVar)
