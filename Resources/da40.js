adc69b1abd95c139c7cb6bd6771bdf843f276aa9
OldGroof <39658757+OldGroof@users.noreply.github.com>
2021-11-15 18:52:21 +0000

    Reduced Logging

--- Resources/da40.js
+++ Resources/da40.js
@@ -487,19 +487,16 @@ function perfTO() {
     let lower = (6.04752 * Math.cos(0.110304 * temp)) + (3.44116 * temp) + 533.674 // 1280
 
     var tod = lower + ((mass - 1280) * ((upper - lower) / 30))
-    console.log("1280-1310: " + tod)
   } else if (mass >= 1200 && mass <= 1280) {
     let upper = (6.04752 * Math.cos(0.110304 * temp)) + (3.44116 * temp) + 533.674 // 1280
     let lower = (6.02705 * Math.cos(0.132884 * temp)) + (2.98482 * temp) + 485.848 // 1200
 
     var tod = lower + ((mass - 1200) * ((upper - lower) / 80))
-    console.log("1200-1280: " + tod)
   } else if (mass >= 1100 && mass < 1200){
     let upper = (6.02705 * Math.cos(0.132884 * temp)) + (2.98482 * temp) + 485.848 // 1200
     let lower = (8.36743 * Math.cos(0.123274 * temp)) + (2.61074 * temp) + 421.793 // 1100
 
     var tod = lower + ((mass - 1100) * ((upper - lower) / 100))
-    console.log("1100-1200: " + tod)
   }
 
   // Calculate wind variation(windVar) based on headwind or tailwind
@@ -509,7 +506,7 @@ function perfTO() {
     var windVar = (-1 * wind) / 2
   }
 
-  var todr = Math.floor((tod + ((0.1 * tod) * windVar)) + 0.5) + 30 // +30m for no wheel fairings and wind correction (+-10% for each windVar)
+  var todr = (Math.floor((tod + ((0.1 * tod) * windVar)) + 0.5)) + 30 // +30m for no wheel fairings and wind correction (+-10% for each windVar)
 
   if (unitTO == "imp") {
     todr = Math.floor((todr * 3.285) + 0.5)
