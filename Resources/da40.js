ee7bb3c87276dfdd024f2ac4764119f2f4028ea8
OldGroof <39658757+OldGroof@users.noreply.github.com>
2021-11-29 20:39:13 +0000

    Arrival metar

--- Resources/da40.js
+++ Resources/da40.js
@@ -199,10 +199,8 @@ function getDepMetar() {
       if (this.readyState == 4 && this.status == 200) {
           result = JSON.parse(this.responseText)
           dateObserved = new Date(result.data[0].observed)
-          console.log(dateObserved)
           oneHourAgo = new Date()
           oneHourAgo.setHours(oneHourAgo.getHours() - 1)
-          console.log(oneHourAgo)
           depMetar = result
 
           if ((depMetar.data[0] != null) && (dateObserved >= oneHourAgo)) {
@@ -213,9 +211,9 @@ function getDepMetar() {
             document.getElementById("txtMetarDep").style.display = "none"
             document.getElementById("manDepEntry").style.display = "block"
 
-            document.getElementById("inpPressDep").disabled = false
-            document.getElementById("inpTempDep").disabled = false
-            document.getElementById("inpWindDep").disabled = false
+            //document.getElementById("inpPressDep").disabled = false
+            //document.getElementById("inpTempDep").disabled = false
+            //document.getElementById("inpWindDep").disabled = false
           }
           document.getElementById("metarBox").style.display = "block"
       }
@@ -230,10 +228,11 @@ function getArrMetar() {
   var metar = new XMLHttpRequest()
   metar.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-          result = JSON.parse(this.responseText)
-          arrMetar = result
+          resultArr = JSON.parse(this.responseText)
+          dateObservedArr = new Date(resultArr.data[0].observed)
+          arrMetar = resultArr
 
-          if (arrMetar.data[0] != null) {
+          if (arrMetar.data[0] != null && dateObservedArr >= oneHourAgo) {
             document.getElementById("txtMetarArr").innerHTML = "METAR " + arrMetar.data[0].raw_text
             document.getElementById("txtMetarArr").style.display = "block"
             document.getElementById("txtTafArr").style.display = "block"
@@ -243,9 +242,9 @@ function getArrMetar() {
             document.getElementById("txtTafArr").style.display = "none"
             document.getElementById("manArrEntry").style.display = "block"
 
-            document.getElementById("inpPressArr").disabled = false
-            document.getElementById("inpTempArr").disabled = false
-            document.getElementById("inpWindArr").disabled = false
+            //document.getElementById("inpPressArr").disabled = false
+            //document.getElementById("inpTempArr").disabled = false
+            //document.getElementById("inpWindArr").disabled = false
           }
           document.getElementById("metarBoxArr").style.display = "block"
       }
